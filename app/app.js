5771fb296c351328052c4528f74b9898ce282109
Petar Stanev <petarstanev@outlook.com>
2016-11-13 14:06:36 +0000

    Added everything from jsbin.

--- app/app.js
+++ app/app.js
@@ -1,17 +1,94 @@
 angular.module('app', [])
+    .controller('mainController', function ($scope) {
+        var cells = [];
+        var playerId = 1;
 
-.controller('mainController', function($scope) {
-  
-  // BUTTONS ======================
-  
-  // define some random object
-  $scope.bigData = {};
-  
-  $scope.bigData.breakfast = false;
-  $scope.bigData.lunch = false;
-  $scope.bigData.dinner = false;
-  
-  // COLLAPSE =====================
-  $scope.isCollapsed = false;
-  
-});
+        $scope.greeting = "Hello World";
+        createCells();
+        $scope.cells = cells;
+
+        $scope.cellClicked = function (id) {
+            playerClick(id);
+        }
+
+
+        function createCells() {
+            for (var i = 0; i < 9; i++) {
+                myCell = { id: i, text: "_", playerId: 0 };
+                cells[i] = myCell;
+            }
+        }
+
+        function playerClick(cellId) {
+            if (playerId === 1) {
+                cells[cellId].text = "X";
+                cells[cellId].playerId = 1;
+            } else {
+                cells[cellId].text = "0";
+                cells[cellId].playerId = 2;
+            }
+            if (checkForWin()) {
+                endGame(false);
+            }
+            changePlayer();
+        }
+
+        function endGame(draw) {
+            if (draw) {
+                $scope.message = "Draw";
+            } else {
+                $scope.message = "Palyer " + playerId + " wins !";
+                $scope.messageClass = "test";
+            }
+        }
+
+        function changePlayer() {
+            if (playerId === 1) {
+                playerId = 2;
+            } else {
+                playerId = 1;
+            }
+        }
+
+        function checkForWin() {
+            if (checkDiagonals()) {
+                return true;
+            } else {
+                for (var i = 0; i < 3; i++) {
+                    if (checkColumns(i)) {
+                        return true;
+                    } else if (checkRows(i * 3)) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        function checkDiagonals() {
+            if (cells[0].playerId === playerId && cells[8].playerId === playerId) {
+                return true;
+            }
+            if (cells[2].playerId === playerId && cells[6].playerId === playerId) {
+                return true;
+            }
+            return false;
+        }
+
+        function checkRows(i) {
+            if (cells[i].playerId === playerId && cells[i + 1].playerId === playerId && cells[i + 2].playerId === playerId) {
+                return true;
+            }
+            return false;
+        }
+
+        function checkColumns(i) {
+            if (cells[i].playerId === playerId && cells[i + 3].playerId === playerId && cells[i + 6].playerId === playerId) {
+                return true;
+            }
+
+            return false;
+        }
+
+    });
